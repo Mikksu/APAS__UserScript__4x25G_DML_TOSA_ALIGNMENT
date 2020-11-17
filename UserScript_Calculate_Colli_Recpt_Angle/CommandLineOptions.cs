b1efc2007275d36c2174869fe340d04eb8f53f7f
Mikk <tw919@qq.com>
2020-11-17 11:58:01 +0800

    在CommandLineOptions的属性Attribute中定义属性默认值。

--- UserScript_Calculate_Colli_Recpt_Angle/CommandLineOptions.cs
+++ UserScript_Calculate_Colli_Recpt_Angle/CommandLineOptions.cs
@@ -17,7 +17,8 @@ namespace UserScript
     [Verb("ry", HelpText = "根据准直Receptacle X轴偏差计算RY修正角度。")]
     public class CalRYOptions : IOptions
     {
-        [Option('p', "pitch", Required = true, HelpText = "通道间间距，单位mm")]
+        [Option('p', "pitch", Required = false, Default = 0.75,
+            HelpText = "通道间间距，单位mm")]
         public double Pitch { get; set; }
 
         [Option('c', "coeff", Required = true, HelpText = "通道间间距换算系数")]
@@ -33,7 +34,8 @@ namespace UserScript
     [Verb("rx", HelpText = "根据准直Receptacle Y轴偏差计算RX修正角度。")]
     public class CalRXOptions : IOptions
     {
-        [Option('p', "pitch", Required = true, HelpText = "通道间间距，单位mm")]
+        [Option('p', "pitch", Required = false, Default = 1,
+            HelpText = "通道间间距，单位mm")]
         public double Pitch { get; set; }
 
         [Option('c', "coeff", Required = true, HelpText = "通道间间距换算系数")]
--- UserScript__4x25G_DML_TOSA_COLLI_LENS/CommandLineOptions.cs
+++ UserScript__4x25G_DML_TOSA_COLLI_LENS/CommandLineOptions.cs
@@ -9,65 +9,70 @@ namespace UserScript
 
         #region 耦合参数定义
 
-        [Option('a', "pth-rascan", Required = false, HelpText = "盲扫退出的阈值功率，单位dBm")]
-        public double PowerThreRectAreaScan { get; set; } = -25;
+        [Option('a', "pth-rascan", Required = false,  Default = -25,
+            HelpText = "盲扫退出的阈值功率，单位dBm")]
+        public double PowerThreRectAreaScan { get; set; }
 
-        [Option('b', "pth-fscan", Required = false, HelpText = "快速焦距扫描退出的阈值功率，单位dBm")]
-        public double PowerThreFocusScan { get; set; } = 0;
+        [Option('b', "pth-fscan", Required = false, Default = 0,
+            HelpText = "快速焦距扫描退出的阈值功率，单位dBm")]
+        public double PowerThreFocusScan { get; set; }
 
-        [Option('d', "fscan-step", Required = false, HelpText = "快速焦距扫描的步进")]
-        public double FocusScanStep { get; set; } = 10;
+        [Option('d', "fscan-step", Required = false, Default = 10,
+            HelpText = "快速焦距扫描的步进")]
+        public double FocusScanStep { get; set; }
 
-        [Option('e', "fscan-range", Required = false, HelpText = "快速焦距扫描的最大移动范围")]
-        public double FocusScanRange { get; set; } = 50;
+        [Option('e', "fscan-range", Required = false,  Default = 50,
+            HelpText = "快速焦距扫描的最大移动范围")]
+        public double FocusScanRange { get; set; }
 
-        [Option('f', "fscan-final-step", Required = false, HelpText = "快速焦距扫描的最小步进，当扫描步进收敛到此值时终止扫描")]
-        public double FocusScanFinalStep { get; set; } = 2;
+        [Option('f', "fscan-final-step", Required = false,  Default = 2,
+            HelpText = "快速焦距扫描的最小步进，当扫描步进收敛到此值时终止扫描")]
+        public double FocusScanFinalStep { get; set; }
 
-        [Option('g', "pth-lscan-n", Required = false,
+        [Option('g', "pth-lscan-n", Required = false, Default = -0.1,
             HelpText = "慢速扫描退出的阈值功率最小值，如果最后两次扫描功率之差大于此值时退出耦合，单位dBm")]
-        public double PowerThreLineScanN { get; set; } = -0.1;
+        public double PowerThreLineScanN { get; set; }
 
-        [Option('i', "pth-lscan-p", Required = false,
+        [Option('i', "pth-lscan-p", Required = false, Default = 0.2, 
             HelpText = "慢速扫描退出的阈值功率最大值，如果最后两次扫描功率之差小于此值时退出耦合，单位dBm")]
-        public double PowerThreLineScanP { get; set; } = 0.2;
+        public double PowerThreLineScanP { get; set; }
 
-        [Option('g', "pth-dual-lscan-n", Required = false,
+        [Option('g', "pth-dual-lscan-n", Required = false, Default = -0.1,
             HelpText = "双边慢速扫描退出的阈值功率最小值，如果最后两次扫描功率之差大于此值时退出耦合，单位dBm")]
-        public double PowerThreDualLineScanN { get; set; } = -0.1;
+        public double PowerThreDualLineScanN { get; set; }
 
-        [Option('k', "pth-dual-lscan-p", Required = false,
+        [Option('k', "pth-dual-lscan-p", Required = false, Default = 0.2,
             HelpText = "双边慢速扫描退出的阈值功率最大值，如果最后两次扫描功率之差小于此值时退出耦合，单位dBm")]
-        public double PowerThreDualLineScanP { get; set; } = 0.2; 
+        public double PowerThreDualLineScanP { get; set; }
 
         #endregion
 
 
         #region 耦合使用的配置文件名定义
 
-        [Option('l', "profilename--focusscan-colli-lens", Required = false,
+        [Option('l', "profilename--focusscan-colli-lens", Required = false, Default = "准直Lens_XY_0.2_20_Z_1_20",
             HelpText = "焦距扫描Lens端扫描使用的配置文件")]
-        public string ProfileNameFocusScanColliLens { get; set; } = "准直Lens_XY_0.2_20_Z_1_20";
+        public string ProfileNameFocusScanColliLens { get; set; }
 
-        [Option('m', "profilename-focusscan-colli-recept", Required = false,
+        [Option('m', "profilename-focusscan-colli-recept", Required = false, Default = "准直Rept_XY_5_200",
             HelpText = "焦距扫描Receptacle端扫描使用的配置文件")]
-        public string ProfileNameFocusScanColliRecept { get; set; } = "准直Rept_XY_5_200";
+        public string ProfileNameFocusScanColliRecept { get; set; }
 
-        [Option('q', "profilename-dual-lscan-lens", Required = false,
+        [Option('q', "profilename-dual-lscan-lens", Required = false, Default = "准直Lens_XY_0.2_10_Z_0.5_10",
             HelpText = "慢速扫描Lens端扫描使用的配置文件")]
-        public string ProfileNameLineScanLens { get; set; } = "准直Lens_XY_0.2_10_Z_0.5_10";
+        public string ProfileNameLineScanLens { get; set; }
 
-        [Option('r', "profilename-dual-lscan-colli-recept", Required = false,
+        [Option('r', "profilename-dual-lscan-colli-recept", Required = false, Default = "准直Rept_XY_5_200",
             HelpText = "双边慢速扫描Receptacle端扫描使用的配置文件")]
-        public string ProfileNameDualLineScanColliRecept { get; set; } = "准直Rept_XY_5_200";
+        public string ProfileNameDualLineScanColliRecept { get; set; }
 
-        [Option('s', "profilename-dual-lscan-colli-lens", Required = false,
+        [Option('s', "profilename-dual-lscan-colli-lens", Required = false, Default = "准直Lens_XY_0.2_10_Z_0.5_10",
             HelpText = "双边慢速扫描Lens端扫描使用的配置文件")]
-        public string ProfileNameDualLineScanColliLens { get; set; } = "准直Lens_XY_0.2_10_Z_0.5_10";
+        public string ProfileNameDualLineScanColliLens { get; set; }
 
-        [Option('t', "profilename-hillclimb", Required = false,
+        [Option('t', "profilename-hillclimb", Required = false, Default = "准直Lens_FineTune",
             HelpText = "爬山扫描使用的配置文件")]
-        public string ProfileNameHillClimb { get; set; } = "准直Lens_FineTune"; 
+        public string ProfileNameHillClimb { get; set; }
 
         #endregion
     }
